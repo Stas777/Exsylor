b7aef79ee9be84b21691d125c61866e67fe09a2a
Golubev Alexandr <lex_golubev@mail.ru>
2014-07-11 22:11:21 +0300

    save and load

--- ExylorQt/Kernel/datatemplate.cpp
+++ ExylorQt/Kernel/datatemplate.cpp
@@ -3,3 +3,32 @@
 DataTemplate::DataTemplate()
 {
 }
+
+void DataTemplate::load(CArch& loader) {
+    loader >> attrCount;
+    for(int i = 0; i < attrCount; i++) {
+        CkAttr attr;
+        attr.Serialize(loader);
+        attributesVector.push_back(attr);
+    }
+    for(int i = 0; i < attrCount; i++) {
+        int domainLenths;
+        loader >> domainLenths;
+        domainLenghtsVector.push_back(domainLenths);
+
+        int domainAddress;
+        loader >> domainAddress;
+        domainAddressVector.push_back(domainAddress);
+    }
+}
+
+void DataTemplate::save(CArch& saver) {
+    saver << attrCount;
+    foreach(CkAttr attr, attributesVector) {
+        attr.Serialize(saver);
+    }
+    for(int i = 0; i < attrCount; i++) {
+        saver << domainLenghtsVector.at(i);
+        saver << domainAddressVector.at(i);
+    }
+}
