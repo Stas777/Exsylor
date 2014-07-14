52f7da64b2be4af4fbb3420c2f6bf569a796d4d0
Golubev Alexandr <lex_golubev@mail.ru>
2014-07-14 13:06:05 +0300

    add atributes in newfile implemented

--- ExylorQt/Gui/newfile.h
+++ ExylorQt/Gui/newfile.h
@@ -2,6 +2,8 @@
 #define NEWFILE_H
 
 #include <QDialog>
+#include <QStandardItemModel>
+#include "../Kernel/Model.h"
 
 namespace Ui {
 class NewFile;
@@ -14,14 +16,20 @@ class NewFile : public QDialog
 public:
     explicit NewFile(QWidget *parent = 0);
     ~NewFile();
+    Model* getModel();
+    QStandardItemModel* attrListViewModel;
+    QStandardItemModel* valueListViewModel;
 
 private slots:
     void on_importButton_clicked();
 
     void on_addButton_clicked();
 
+    void on_attributeslistView_clicked(const QModelIndex &index);
+
 private:
     Ui::NewFile *ui;
+    Model* model;
 };
 
 #endif // NEWFILE_H
